2ba9091320564ed72a7ab1c38b188b3c92ebfeba
Adam Lopez <adamf04@gmail.com>
2022-11-20 02:47:25 -0500

    organizing code for the admin dashboard and the user logged in dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,16 +1,22 @@
-<x-app-layout>
-
-</x-app-layout>
- 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+    @include('admin.css')
 </head>
-<body>
-    <h1>ayo</h1>
+
+<body class="bg-theme bg-theme1">
+    <!-- Start wrapper-->
+    <div id="wrapper">
+        <!--Start sidebar-wrapper-->
+        @include('admin.sidebar')
+        <!--End sidebar-wrapper-->
+        <!--Start topbar header-->
+        @include('admin.header')
+        <!--End topbar header-->
+        <div class="clearfix"></div>
+        @include('admin.body')
+    </div>
+    <!--End wrapper-->
+    @include('admin.script')
 </body>
 </html>
